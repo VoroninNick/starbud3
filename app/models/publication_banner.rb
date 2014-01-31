8437c1bbf67005eafcf481cb126e9ff51904ba9e
TortronUA <nazariy.papizh@gmail.com>
2014-01-31 12:22:24 +0200

    pre release testing 1

--- app/models/publication_banner.rb
+++ app/models/publication_banner.rb
@@ -1,3 +1,51 @@
 class PublicationBanner < ActiveRecord::Base
+  attr_accessible :title, :image, :short_description, :description, :index_sorting, :link
 
+  has_attached_file :image, styles:{
+      large: "1800x600>"
+  },url:'/assets/images/publication_banner/:id/image_:style.:extension',
+                    path:':rails_root/public:url'
+
+
+  rails_admin do
+    navigation_label 'Публікації'
+    label 'Банер'
+    label_plural 'Слайди'
+
+    list do
+      field :title
+      field :image
+      field :short_description
+      #field :description
+      field :index_sorting
+      field :link
+    end
+    edit do
+      field :title do
+        label ''
+        help ''
+      end
+      field :image do
+        label ''
+        help 'Розмір зображення 1800x600'
+      end
+      field :short_description do
+        label ''
+        help ''
+      end
+      #field :description do
+      #  label ''
+      #  help ''
+      #end
+      field :index_sorting do
+        label ''
+        help ''
+      end
+      field :link do
+        label ''
+        help ''
+      end
+
+    end
+  end
 end
