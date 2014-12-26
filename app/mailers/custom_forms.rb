eb4dbba747db59d490a85d6342f7496db3d114fb
Nazariy <nazariy.papizh@gmail.com>
2014-12-26 18:42:12 +0200

    fixed bug with send mail from call order form

--- app/mailers/custom_forms.rb
+++ app/mailers/custom_forms.rb
@@ -1,5 +1,5 @@
 class CustomForms < ActionMailer::Base
-  default from: "Star-Bud <support@voroninstudio.eu>"
+  default from: "Star Bud <support@voroninstudio.eu>"
   default to: 	"nazariy.papizh@gmail.com"
   def call_order_data(name, phone, call_time_begin, call_time_end)
     @name = name
