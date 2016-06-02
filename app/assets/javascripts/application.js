1f4e453c19831d8545a3ee244da2af3af82690f0
Nazariy Papizh <nazariy.papizh@gmail.com>
2016-06-02 12:32:02 +0300

    cleared js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,34 +10,44 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= require jquery-1.10.2.min
+//= require jquery-1.10.2.min
+
+
+
 //= require jquery
 //= require jquery_ujs
 
-//last update
-//= require jquery-1.10.2.min
-//= require jquery-1.10.2.min
+
+
+
+
+
+
+
+//= require visual_designer
+
+
 //= require jquery.bxslider.min
-//= require excanvas.compiled
-//= require get_viewport
-//= require jquery.cookie
-//= require jquery.easing.1.3
 //= require jquery.easytabs
-//= require jquery.fancybox
+//= require jquery.cookie
 //= require jquery.flexisel
 //= require jquery.treeview
+//= require jquery.easing.1.3
+//= require jquery.fancybox
+//= require get_viewport
+//= require excanvas.compiled
+
+//= require main
+
+//= require publications
+//= require about_articles
+//= require catalog
+
+
 //= require jquery.validate.min
 //= require jquery.validationEngine-ru
 
-//= require about_articles
-//= require catalog
 //= require contacts
 
-
-
-//= require publications.js
-//= require visual_designer.js
-//= require main.js
-
-
-
-// require_tree .
