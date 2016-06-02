5ba5db2877c5a469d5a27b7ce1eb7272aefd6182
Nazariy Papizh <nazariy.papizh@gmail.com>
2016-06-02 12:03:16 +0300

    remove required tree

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,31 @@
 //= require jquery
 //= require jquery_ujs
 
+//last update
+//= require jquery-1.10.2.min
+//= require jquery-1.10.2.min
+//= require jquery.bxslider.min
+//= require excanvas.compiled
+//= require get_viewport
+//= require jquery.cookie
+//= require jquery.easing.1.3
+//= require jquery.easytabs
+//= require jquery.fancybox
+//= require jquery.flexisel
+//= require jquery.treeview
+//= require jquery.validate.min
+//= require jquery.validationEngine-ru
 
-//= require_tree .
+//= require about_articles
+//= require catalog
+//= require contacts
+
+
+
+//= require publications.js
+//= require visual_designer.js
+//= require main.js
+
+
+
+// require_tree .
